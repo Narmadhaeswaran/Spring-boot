c11d2e34c6f07a8392c9e5569d781bfdce11d6a7
Narmadhaeswaran <109136473+Narmadhaeswaran@users.noreply.github.com>
2022-08-01 16:14:11 +0530

    Add files via upload

---
+++ src/main/java/com/mayuratech/CustomerTask/CustomerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mayuratech.CustomerTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTaskApplication.class, args);
+	}
+
+}
